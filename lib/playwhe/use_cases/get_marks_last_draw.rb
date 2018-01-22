96b486d8f20ff4c4cce0fff2b0056d16226e63b4
Dwayne R. Crooks <me@dwaynecrooks.com>
2018-01-22 08:45:05 -0400

    Order results for /stats/marks-last-draw correctly
    When the dates are equal we order the periods in ascending order,
    i.e. EM, AM, AN and then PM.

--- lib/playwhe/use_cases/get_marks_last_draw.rb
+++ lib/playwhe/use_cases/get_marks_last_draw.rb
@@ -14,8 +14,25 @@ module PlayWhe
 
         {
           self: '/stats/marks-last-draw',
-          results: results.sort { |r1, r2| r1[:date] <=> r2[:date] }
+          results: results.sort { |a, b| compare(a, b) }
         }
+      end
+
+      private
+
+      ASC_PERIOD = {
+        'EM' => 0,
+        'AM' => 1,
+        'AN' => 2,
+        'PM' => 3
+      }
+
+      def compare(a, b)
+        if a[:date] == b[:date]
+          ASC_PERIOD[a[:period]] <=> ASC_PERIOD[b[:period]]
+        else
+          a[:date] <=> b[:date]
+        end
       end
     end
   end
